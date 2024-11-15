d14e8db82302c0b53bbf209131a07f9b336163d7
suchitkumarchennuri <143919454+suchitkumarchennuri@users.noreply.github.com>
2024-11-15 17:00:40 +0000

    Initial commit.

---
+++ products.js
@@ -0,0 +1,19 @@
+// products.js
+const fs = require('fs').promises
+const path = require('path')
+
+const productsFile = path.join(__dirname, 'data/products.json')
+
+module.exports = {
+  list
+}
+
+
+/**
+ * List all products
+ * @returns {Promise<Array>}
+ */
+async function list () {
+  const data = await fs.readFile(productsFile)
+  return JSON.parse(data)
+}
