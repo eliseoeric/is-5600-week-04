c0696d5259b4f436ad8fa4919bbee8f19cb46058
Pallavicheeti <pallavi.gosu@slu.edu>
2024-11-19 22:55:53 +0000

    lab4 submission

--- api.js
+++ api.js
@@ -1,61 +1,55 @@
-const fs = require('fs').primises
 const path = require('path')
 const Products = require('./products')
 const autoCatch = require('./lib/auto-catch')
-/**
+
+
+ /**
  * Handle the root route
  * @param {object} req
  * @param {object} res
 */
-function handleRoot(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
-  }
-  
-  /**
-   * List all products
-   * @param {object} req
-   * @param {object} res
-   */
-  async function listProducts (req, res) {
+function handleRoot (req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+}
 
-    // Extract the limit and offset query parameters
-    const { offset = 0, limit = 25, tag } = req.query
-  
-    try {
-      // Pass the limit and offset to the Products service
-      res.json(await Products.list({
-        offset: Number(offset),
-        limit: Number(limit),
-        tag,
-      }))
-    } catch (err) {
-      res.status(500).json({ error: err.message })
-    }
-  }
 
-  async function getProduct (req, res, next) {
-  
-    const { id } = req.params
-  
-    try {
-      const product = await Products.get(id)
-      if (!product) {
-        // next() is a callback that will pass the request to the next available route in the stack
-        return next()
-      }
-  
-      return res.json(product)
-    } catch (err) {
-      res.status(500).json({ error: err.message })
-    }
+async function getProduct (req, res, next) {
+  const { id } = req.params
+  const product = await Products.get(id)
+  if (!product) {
+    return next()
   }
+  
+  return res.json(product)
+}
+
+
+/**
+ * List all products
+ * @param {object} req
+ * @param {object} res
+ */
+async function listProducts (req, res) {
+  // Extract the limit and offset query parameters
+  const { offset = 0, limit = 25, tag } = req.query
+  // Pass the limit and offset to the Products service
+  res.json(await Products.list({
+    offset: Number(offset),
+    limit: Number(limit),
+    tag
+  }))
+  
+}
+
 async function createProduct (req, res) {
-    console.log('request body:', req.body)
-    res.json(req.body)
-  }
-  module.exports = autoCatch({
-    handleRoot,
-    listProducts,
-    getProduct,
-    createProduct
-  });
+  console.log('request body:', req.body)
+  res.json(req.body)
+}
+
+
+module.exports = autoCatch({
+  handleRoot,
+  listProducts,
+  getProduct,
+  createProduct,
+});
