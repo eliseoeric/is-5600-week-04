520a1ea8c20a693acbcb34143a771e13ec06bcea
Arya003-slu <aryavardhanreddy.challa@slu.edu>
2024-11-19 22:56:20 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const fs = require('fs').promises
 const path = require('path')
 const express = require('express')
+const api = require('./api')
+
 
 // Set the port
 const port = process.env.PORT || 3000
@@ -9,8 +11,10 @@ const app = express()
 // Register the public directory
 app.use(express.static(__dirname + '/public'));
 // register the routes
-app.get('/products', listProducts)
-app.get('/', handleRoot);
+app.get('/products', api.listProducts)
+
+app.get('/', api.handleRoot)
+
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
 
