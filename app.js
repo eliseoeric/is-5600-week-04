0635514e269bb84cf5263ca03df791bbf738eeea
Arya003-slu <aryavardhanreddy.challa@slu.edu>
2024-11-19 23:28:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const express = require('express')
 const api = require('./api')
+const middleware = require('middleware')
 
 
 // Set the port
@@ -9,10 +10,11 @@ const port = process.env.PORT || 3000
 const app = express()
 // Register the public directory
 app.use(express.static(__dirname + '/public'));
-// register the routes
-app.get('/products', api.listProducts)
 
+
+app.use(middleware.cors)
 app.get('/', api.handleRoot)
+app.get('/products', api.listProducts)
 app.get('/products/:id', api.getProduct)
 // Boot the server
 app.listen(port, () => console.log(`Server listening on port ${port}`))
